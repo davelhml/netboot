9d3f06bf448a89b5ed2ccc7d6675de959b78f02f
Jing Li <lijing@example.com>
2018-05-01 22:42:57 +0800

    install nova

---
+++ controller/scripts/nova-init.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+source /root/adminrc
+openstack user create --domain default --password cc nova
+openstack role add --project service --user nova admin
+openstack service create --name nova --description "OpenStack Compute" compute
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2.1
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2.1
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2.1
+
+openstack user create --domain default --password cc placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://controller:8778
+openstack endpoint create --region RegionOne placement internal http://controller:8778
+openstack endpoint create --region RegionOne placement admin http://controller:8778
