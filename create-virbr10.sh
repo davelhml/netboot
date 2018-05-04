3bde2b7fe0f6f2135ad005f8f79ba8f1a0ab73a6
Jing Li <lijing@example.com>
2018-05-04 00:19:33 -0400

    modify create-virbr10.sh

--- create-virbr10.sh
+++ create-virbr10.sh
@@ -3,20 +3,30 @@
 set -ex
 
 mac=$(hexdump -vn3 -e '/3 "52:54:00"' -e '/1 ":%02x"' -e '"\n"' /dev/urandom)
+
+
+# cleanup
+ip link set down dev virbr10 | /bin/true
+brctl delbr virbr10 | /bin/true
+ip link del virbr10-dummy | | /bin/true
+if [ -f /var/run/dnsmasq-virbr10.pid ]; then
+    kill $(cat /var/run/dnsmasq-virbr10.pid)
+fi
+
+
 ip link add virbr10-dummy address $mac type dummy
-ip link show virbr10-dummy
 
 # Create a virtual bridge
 brctl addbr virbr10
 brctl stp virbr10 on
 brctl addif virbr10 virbr10-dummy
 ip address add 192.168.100.1/24 dev virbr10 broadcast 192.168.100.255
-ifconfig virbr10 up
+ip link set up virbr10
 
-## nat ##
+# SNAT
 iptables -t nat -A POSTROUTING -s 192.168.100.0/24 ! -d 192.168.100.0/24 -j MASQUERADE
 
-## DNSMASQ
+# DNSMASQ
 mkdir -p /var/lib/dnsmasq/virbr10
 touch /var/lib/dnsmasq/virbr10/hostsfile
 touch /var/lib/dnsmasq/virbr10/leases
